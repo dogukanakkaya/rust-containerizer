ad9df15a6d2085924acd8395421da3ee09ebf0fe
dogukanakkaya <doguakkaya27@hotmail.com>
2022-02-21 21:04:07 +0300

    feat: get driver options from args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::collections::HashMap;
+
+mod drivers;
+
 fn main() {
-    println!("Hello, world!");
+    let mut driver_options = HashMap::new();
+
+    for arg in env::args() {
+        if arg.starts_with("--") {
+            match arg.find('=') {
+                Some(i) => driver_options.insert(arg[2..i].to_string(), arg[i + 1..].to_string()),
+                _ => None
+            };
+        }
+    }
+    println!("driver = {:?}", driver_options);
 }
