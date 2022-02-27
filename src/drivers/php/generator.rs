0f2e0ad5eb82dbee35d8f85a0b6f370f65ca1d23
dogukanakkaya <doguakkaya27@hotmail.com>
2022-02-27 15:33:42 +0300

    feat: add from php to dockerfile

--- src/drivers/php/generator.rs
+++ src/drivers/php/generator.rs
@@ -10,10 +10,16 @@ impl Generator {
         // used extensions can be found in composer.json's require object like "ext-pdo", "ext-mongo"
         let composer = Composer::new(format!("{}/composer.json", project_path), 2);
 
-        let mut version = composer.data()["require"]["php"].as_str().unwrap_or_else(|| "latest");
-
-        println!("PHP Version: {}", version);
+        let php_version = composer.data()["require"]["php"].as_str().unwrap_or_else(|| "latest")
+        .replace("<", "")
+        .replace(">", "")
+        .replace("=", "")
+        .replace("^", "")
+        .replace("~", "");
+        //let php_version = php_version.chars().filter(|x| !vec!['<', '>', '=', '^', '~'].contains(x)).collect::<String>();
                          
-        let mut dockerfile_contents = b"FROM php{}-fpm";
+        let mut dockerfile_contents = format!("FROM php:{}-fpm", php_version);
+
+        println!("Dockerfile: \n{}", dockerfile_contents);
     }
 }
