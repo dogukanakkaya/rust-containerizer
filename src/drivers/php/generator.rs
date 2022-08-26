f4cabd3343a287a8b9a752c21fa6c39be52f0801
dogukanakkaya <doguakkaya27@hotmail.com>
2022-08-27 02:30:49 +0300

    chore: lint & format

--- src/drivers/php/generator.rs
+++ src/drivers/php/generator.rs
@@ -1,45 +1,58 @@
 use super::composer::Composer;
-use crate::drivers::{js::package::Package, generator::Generator};
+use crate::drivers::{generator::Generator, js::package::Package};
 use std::{fs::File, io::Write};
 
 const SUPPORTED_IMAGES: [&'static str; 4] = ["elasticsearch", "redis", "mysql", "postgresql"];
 
-pub struct PHPGenerator { }
+pub struct PHPGenerator {}
 
 impl PHPGenerator {
     fn generate_composer(composer: Composer) -> String {
-        let php_version = composer.data()["require"]["php"].as_str().unwrap_or_else(|| "latest").chars().filter(|x| !vec!['<', '>', '=', '^', '~'].contains(x)).collect::<String>();
+        let php_version = composer.data()["require"]["php"]
+            .as_str()
+            .unwrap_or("latest")
+            .chars()
+            .filter(|x| !vec!['<', '>', '=', '^', '~'].contains(x))
+            .collect::<String>();
 
         // find used extensions in composer.json's require object like "ext-pdo", "ext-mongo"
         let mut extensions = vec![];
 
         for require in composer.data()["require"].as_object().unwrap().iter() {
             let (key, value) = require;
-            
+
             if key.starts_with("ext-") && value == "*" {
                 extensions.push(&key[4..]);
             }
+
+            // check for known packages like elasticsearch/elasticsearch and create a compose file for those
         }
 
         format!(
             "
-            FROM php:{}-fpm\n
-            WORKDIR /var/www/php\n
-            RUN apt-get update\n
-            RUN docker-php-ext-install {}\n
+            FROM php:{}-fpm
+            WORKDIR /var/www/php
+            RUN apt-get update
+            RUN docker-php-ext-install {}
             ",
             php_version,
             extensions.join(" ")
         )
     }
 
-    fn generate_package(package: Package) -> String {  
-        let node_version = package.data()["engines"]["node"].as_str().unwrap_or_else(|| "16").chars().filter(|x| !vec!['<', '>', '=', '^', '~'].contains(x)).collect::<String>();
+    fn generate_package(package: Package) -> String {
+        let node_version = package.data()["engines"]["node"]
+            .as_str()
+            .unwrap_or("16")
+            .chars()
+            .filter(|x| !vec!['<', '>', '=', '^', '~'].contains(x))
+            .collect::<String>();
 
         // find node version from package.json or somehow if can't be found by package.json
         format!(
             "
-            RUN curl -fsSL https://deb.nodesource.com/setup_{}.x | bash -\nRUN apt-get-install -y nodejs
+            RUN curl -fsSL https://deb.nodesource.com/setup_{}.x | bash -
+            RUN apt-get-install -y nodejs
             ",
             &node_version[..node_version.find(".").unwrap()]
         )
@@ -58,12 +71,12 @@ impl Generator for PHPGenerator {
 
         match package {
             Ok(pck) => dockerfile_contents.push_str(Self::generate_package(pck).as_str()),
-            _ => ()
+            _ => (),
         }
 
         match dockerfile.write_all(dockerfile_contents.as_bytes()) {
             Ok(()) => println!("Dockerfile generated at: {}", project_path),
-            Err(_) => unimplemented!()
+            Err(_) => unimplemented!(),
         }
     }
 }
